5684ad18c06de775c4719a156483dc0e5fa79667
Adnan <cs0068adnan@gmail.com>
2023-10-11 22:21:56 +0530

    final_commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
-    const iconsize = 30 ;
+  const iconsize = 30;
   return (
     <footer className="container-fluid p-3 bg-dark h-100">
+      <div className="my-2 text-secondary text-center">
+        Showcase: Unveiling Extraordinary Talent, Crafting Unforgettable
+        Moments, and Elevating Entertainment Experiences
+      </div>
       <div className="footer-content">
-        <div className="social-icons text-center mb-2">
-          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className='mx-3'>
-            <FaFacebook size={iconsize} color='grey'/>
+        <div className="social-icons text-center mb-2 ">
+          <a
+            href="https://www.facebook.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-3"
+          >
+            <FaFacebook size={iconsize} color="grey" />
           </a>
-          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer" className='mx-3'>
-            <FaTwitter size={iconsize} color='grey' />
+          <a
+            href="https://twitter.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-3"
+          >
+            <FaTwitter size={iconsize} color="grey" />
           </a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className='mx-3'>
-            <FaInstagram size={iconsize} color='grey' />
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-3"
+          >
+            <FaInstagram size={iconsize} color="grey" />
           </a>
-          <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer" className='mx-3'>
-            <FaLinkedin size={iconsize} color='grey' />
+          <a
+            href="https://www.linkedin.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-3"
+          >
+            <FaLinkedin size={iconsize} color="grey" />
           </a>
         </div>
-        <p className='text-center text-light'>&copy; 2023 SHOWCASE. All rights reserved.</p>
+        <p className="text-center text-light">
+          &copy; 2023 SHOWCASE. All rights reserved.
+        </p>
       </div>
     </footer>
   );
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from "react";
 import { useAnimation, motion } from "framer-motion";
+import ScrollReveal from "scrollreveal";
 import { useInView } from "react-intersection-observer";
 import Footer from "./Footer";
 
 const Home = () => {
   const text = "Top Categories";
   const ctrls = useAnimation();
+
   const { ref, inView } = useInView({
     threshold: 0.5,
     triggerOnce: false,
@@ -23,7 +25,7 @@ const Home = () => {
       opacity: 1,
       y: `0em`,
       transition: {
-        duration: 1,
+        duration: 0.5,
         ease: [0.2, 0.65, 0.3, 0.9],
       },
     },
@@ -55,6 +57,43 @@ const Home = () => {
       ctrls.start("hidden");
     }
   }, [ctrls, inView]);
+  useEffect(() => {
+    ScrollReveal().reveal(".headline", {
+      origin: 'bottom',
+      distance: '20px',
+      duration: 800,
+      delay: 200,
+      easing: 'cubic-bezier(0.68, -0.55, 0.27, 1.55)',
+      reset:true,
+    });
+    ScrollReveal().reveal(".top-cat", {
+      origin: 'bottom',
+      distance: '20px',
+      duration: 800,
+      delay: 200,
+      easing: 'cubic-bezier(0.68, -0.55, 0.27, 1.55)',
+      reset: true,
+      
+    });
+    ScrollReveal().reveal(".tagline", {
+      origin: 'right',
+      distance: '20px',
+      duration: 1000,
+      delay: 400,
+      easing: 'cubic-bezier(0.68, -0.55, 0.27, 1.55)',
+      reset:true,
+    });
+    ScrollReveal().reveal(".gif", {
+      origin: 'right',
+      distance: '20px',
+      duration: 800,
+      delay: 600,
+      easing: 'cubic-bezier(0.68, -0.55, 0.27, 1.55)',
+     
+    });
+    return () => ScrollReveal().destroy();
+  },[]);
+
   return (
     <>
       <div className="bg-dark">
@@ -122,8 +161,8 @@ const Home = () => {
           </button>
         </div>
       </div>
-      <div className="container-fluid vh-100 ">
-        <motion.h2 className="my-3" aria-label={text} role="heading">
+      <div className="container-fluid h-100 ">
+        <motion.h2 className="my-3 top-cat" aria-label={text} role="heading">
           {text.split("").map((word, index) => (
             <motion.span
               ref={ref}
@@ -147,7 +186,7 @@ const Home = () => {
                   aria-hidden="true"
                   key={index}
                   variants={characterAnimation}
-                  style={{ display: "inline-block", "margin-right": "-0.05em" }}
+                  style={{ display: "inline-block", "margin-right": "-0.2em" }}
                 >
                   {character}
                 </motion.span>
@@ -218,6 +257,147 @@ const Home = () => {
             </div>
           </motion.div>
         </motion.div>
+        <section className="text-center bg-light testimonials">
+          <div className="container">
+            <h2 className="mb-5">What people are saying...</h2>
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="mx-auto testimonial-item test-1 mb-5 mb-lg-0">
+                  <img
+                    className="rounded-circle img-fluid mb-3"
+                    src="testimonials-1.jpg"
+                    alt="Testimonial 1"
+                  />
+                  <h5>Margaret E.</h5>
+                  <p className="font-weight-light mb-0">
+                    <br />
+                    "Working with Showcase made event planning a breeze! The
+                    variety of talents and seamless booking process exceeded my
+                    expectations."
+                    <br />
+                    <br />
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="mx-auto testimonial-item test-2 mb-5 mb-lg-0">
+                  <img
+                    className="rounded-circle img-fluid mb-3"
+                    src="testimonials-2.jpg"
+                    alt="Testimonial 2"
+                    width="192"
+                    height="192"
+                  />
+                  <h5>Fred S.</h5>
+                  <p className="font-weight-light mb-0">
+                    "Showcase provides an incredible platform for artists to
+                    connect with their audience. The exposure I gained has been
+                    instrumental in my career."
+                  </p>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="mx-auto testimonial-item test-3 mb-5 mb-lg-0">
+                  <img
+                    className="rounded-circle img-fluid mb-3"
+                    src="testimonials-3.jpg"
+                    alt="Testimonial 3"
+                  />
+                  <h5>Sarah W.</h5>
+                  <p className="font-weight-light mb-0">
+                    "I love the simplicity and effectiveness of Showcase. It has
+                    allowed me to share my music with a broader audience, and
+                    I've had amazing opportunities come my way."
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="visible"
+          className="h-100 row rounded mx-1"
+        >
+          <div className=" my-3 col-sm-12 col-lg-8 my-2 p-0 h-100 ">
+            {" "}
+            <motion.iframe
+              whileTap={{ scale: 0.8 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+              variants={item}
+              className="card rounded mx-auto d-block img-fluid w-100 youtube-embed"
+              src="https://www.youtube.com/embed/223eI1x6PsQ?si=oqbiNBj2xmJPrgsK?autoplay=1&mute=1"
+              title="YouTube Video"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+              allowfullscreen
+            ></motion.iframe>
+          </div>
+          <div className="my-3 col-sm-12 col-lg-4">
+            <h1 className="headline">
+              Discover, Book, and Elevate: Showcase Your Talent, Craft
+              Unforgettable Events!
+            </h1>
+            <p className="tagline my-2">
+              <h3>
+                Empowering Artists, Enriching Experiences: Where Talent Meets
+                Opportunity in Every Spotlight
+              </h3>
+            </p>
+          </div>
+        </motion.div>
+        <section class="py-4 py-xl-5">
+          <div class="row d-flex justify-content-between">
+            <div className="col-md-6">
+              <img src="feedback.gif" className="gif" alt="" />
+            </div>
+            <div class="col-md-6 feed-form">
+              <div class="card border-0 mb-5">
+                <div class="card-body p-sm-5">
+                  <h2 class="text-center mb-4">Contact us</h2>
+                  <form method="post">
+                    <div class="mb-3">
+                      <input
+                        id="name-2"
+                        class="form-control"
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div class="mb-3">
+                      <input
+                        id="email-2"
+                        class="form-control"
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div class="mb-3">
+                      <textarea
+                        id="message-2"
+                        class="form-control"
+                        name="message"
+                        rows="6"
+                        placeholder="Message"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button
+                        class="btn btn-primary d-block w-100"
+                        type="submit"
+                      >
+                        Send{" "}
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
       <Footer />
     </>
